a7c456504c3a5469e4085cbd71e88fda285746f3
thongdeptraiwa <thong442001@gmail.com>
2025-03-03 18:07:56 +0700

    3/3-fix-port

--- index.js
+++ index.js
@@ -112,7 +112,7 @@ app.use('/noti', notiRoute);
 // });
 
 // Khởi động server
-const PORT = 3001;
+const PORT = 4000;
 server.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
