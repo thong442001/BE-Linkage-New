60e5199338a72ad91919c011a5b7121958e102af
thongdeptraiwa <thong442001@gmail.com>
2025-03-07 16:57:28 +0700

    7/3-3

--- controllers/relationshipController.js
+++ controllers/relationshipController.js
@@ -70,8 +70,7 @@ async function guiLoiMoiKetBan(ID_relationship, me) {
         // Tìm quan hệ giữa hai người
         const relation = await relationship.findById(ID_relationship)
             .populate('ID_userA', 'first_name last_name avatar')
-            .populate('ID_userB', 'first_name last_name avatar')
-            .lean();
+            .populate('ID_userB', 'first_name last_name avatar');
 
         if (!relation || relation.relation !== 'Người lạ') {
             return false;
@@ -80,12 +79,12 @@ async function guiLoiMoiKetBan(ID_relationship, me) {
         let newRelationStatus = "";
         let receiverId = ""; // Người nhận lời mời kết bạn
 
-        if (relation.ID_userA == me) {
+        if (relation.ID_userA._id.toString() === me.toString()) {
             newRelationStatus = 'A gửi lời kết bạn B';
-            receiverId = relation.ID_userB; // Người nhận là B
-        } else if (relation.ID_userB == me) {
+            receiverId = relation.ID_userB._id.toString(); // Người nhận là B
+        } else if (relation.ID_userB._id.toString() === me.toString()) {
             newRelationStatus = 'B gửi lời kết bạn A';
-            receiverId = relation.ID_userA; // Người nhận là A
+            receiverId = relation.ID_userA._id.toString(); // Người nhận là A
         } else {
             return false;
         }
@@ -94,19 +93,20 @@ async function guiLoiMoiKetBan(ID_relationship, me) {
         relation.relation = newRelationStatus;
         await relation.save();
 
-        // tạo notification
-        const notificationItem = {
+        // Tạo notification
+        const notificationItem = new notification({
             ID_relationship: relation._id,
             ID_user: receiverId,
-            content: me == relation.ID_userA
+            content: me.toString() === relation.ID_userA._id.toString()
                 ? `${relation.ID_userA.first_name} ${relation.ID_userA.last_name} đã gửi lời mời kết bạn với bạn`
                 : `${relation.ID_userB.first_name} ${relation.ID_userB.last_name} đã gửi lời mời kết bạn với bạn`,
             type: 'Lời mời kết bạn',
-        }
-        const newNotification = await notification.create(notificationItem);
+        });
+
+        await notificationItem.save();
 
         // Gửi thông báo cho người nhận lời mời
-        await guiThongBaoKetBan(receiverId, newNotification);
+        await guiThongBaoKetBan(receiverId, notificationItem);
 
         return relation;
     } catch (error) {
@@ -114,6 +114,8 @@ async function guiLoiMoiKetBan(ID_relationship, me) {
         throw error;
     }
 }
+
+
 
 // 🛠 Hàm gửi thông báo kết bạn
 async function guiThongBaoKetBan(ID_user, notifi) {
