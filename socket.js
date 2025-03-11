fd6b32aae627339d99a294f05352d0e854ccf80d
thongdeptraiwa <thong442001@gmail.com>
2025-03-11 15:26:10 +0700

    11/3-9

--- socket.js
+++ socket.js
@@ -145,7 +145,7 @@ function setupSocket(server) {
             //     .filter(({ token }) => token && token.trim().length > 0); // Lọc token hợp lệ
 
             const messages = [];
-            fcmData.forEach(({ ID_user, tokens }) => {
+            fcmTokens.forEach(({ ID_user, tokens }) => {
                 if (tokens && tokens.length > 0) {
                     tokens.forEach(token => {
                         messages.push({
