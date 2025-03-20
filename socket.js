a856e610c9aa3fa75113f31ffde3e4c53486fe2c
thongdeptraiwa <thong442001@gmail.com>
2025-03-20 14:53:40 +0700

    20/3-game-3la-5

--- socket.js
+++ socket.js
@@ -204,7 +204,7 @@ function setupSocket(server) {
                     },
                 );
             }
-            io.to(ID_group).emit('lang-nghe-moi-choi-game-3-la');
+            // io.to(ID_group).emit('lang-nghe-moi-choi-game-3-la');
         });
         socket.on('chap-nhan-choi-game-3-la', async (data) => {
             const { ID_group } = data;
