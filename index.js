33f885a4074658633e9e47816383975e4ed435c1
thongdeptraiwa <thong442001@gmail.com>
2025-04-12 16:39:09 +0700

    12/4-fix-api-addPost-2

--- index.js
+++ index.js
@@ -167,7 +167,7 @@ app.use('/gmail_otp', gmail_otpRoute);
 
 
 // Khởi động server
-const PORT = 3001;
+const PORT = 4000;
 server.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
